562d17139ff95496ec7ed2c103b48f7dfd0fedaa
Maksim M <ximerik@ex.ua>
2015-12-15 16:35:30 +0200

    Update trainData.h

--- trainData.h
+++ trainData.h
@@ -11,24 +11,32 @@ class Traindata
 public:
     Traindata(char*);
     ~Traindata();
+    void dataVectors();             // распределение данных по векторам
     void linReg();                  // Функция осущ линейную регрессию
     void normData();                // Нормализация данных
     void freeVar();                 // Матрица свободных переменных
     void freeVarT();                // Транспонированая матрица свободных переменных
     void multMatrx();               // Перемножение матриц
-    void determ();                  // Определитель матрицы
-    void setData();                 // Функция в которой выполняются операции над данными
-    void getData();                 // Функция вывода результата
+    void invrMatrx();               // Обратная матрицы
 
 private:
-    vector <int> inputTime;            // Вектор с данными времени
-    vector <float> timeNorm;
-    vector <float> avgChek;            // Вектор с данными о среднем чеке
-    vector <float> avgchekNorm;
-    vector <float> profit;             // Вектор с данными о чистой прибыли
-    vector <float> profitNorm;
-    float *A, **tranA, **matrixMult;
-    int     inputDataArraySize;        // Размер массива обрабатываемых данных. Нужен для работы счетчиков
+    std::vector <float> inputTime;            // Вектор с данными времени
+    std::vector <float> timeNorm;
+    std::vector <float> avgChek;            // Вектор с данными о среднем чеке
+    std::vector <float> avgchekNorm;
+    std::vector <float> profit;             // Вектор с данными о чистой прибыли
+    std::vector <float> profitNorm;
+    std::vector <float> customer;            // вектор покупателей
+    std::vector <float> customerNorm;
+    float   **mfreeVar,                     // Матрица свободной переменной
+            **mfreeVarT,                    // транспонированая Матрица свободной переменной
+            **matrixMult,                   // результирующая матрица mfreeVarT*mfreeVar
+            **invMatrix,                    // обратная матрица matrixMult
+            **dataTable;                    // массив со вводными данными
+    int     rows,
+            cols,
+            freeVar_number;                 // количество свободных переменных
+    size_t inputDataArraySize;        // Размер массива обрабатываемых данных. Нужен для работы счетчиков
 };
 
 
