788785fe33528d35e515ecb19781eb5b4896a866
Maksim M <ximerik@ex.ua>
2015-12-24 17:21:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,24 @@
+/**
+// ==Abstract==
+//
+// Linear regression - method of establishing the relationship between the two variables.
+// Below is an example of a program that builds a linear model based on a given sample
+// and displays the result on the chart.
+//
+// Model:
+// y = Omega1 + Omega2*Xi + Omega2*Xj+...+OmegaN*Xn + Ei + Ej +...+En
+//
+// Input parameters:
+// Omega1, Omega2,...OmegaN  - weights;
+// Xi, Xj...Xn               - free variables: time, number of customers etc.
+// Ei...En                   - random value. It is assumed that the random variable is
+//                             normally distributed with zero math expectancy and
+//                             fixed variance.
+// Output
+// y - average check.
+//
+*/
+
 #include <iostream>
 #include "trainData.h"
 
@@ -12,6 +33,7 @@ int main()
     obj.freeVar();
     obj.freeVarT();
     obj.multMatrx();
-    //obj.invrMatrx();
+    obj.inverceMatrix();
+    obj.linear_Regresion();
     return 0;
 }
